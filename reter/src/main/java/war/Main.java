cf11329487eebadef7bfe3705873ce5b6c11bd8a
swaseemahmed <41055503+swaseemahmed@users.noreply.github.com>
2018-08-18 01:28:30 +0530

    Add files via upload

---
+++ reter/src/main/java/war/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package war;
+
+/**
+ * 
+ * 
+ */
+public class Main {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
